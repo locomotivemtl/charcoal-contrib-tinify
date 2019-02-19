4e03961ae5c94492d734ac2ee8a0c4d596e21af9
Joel Alphonso <joel@locomotive.ca>
2019-02-18 20:17:10 -0500

    Adding base path to tinify config

--- src/Charcoal/Tinify/TinifyConfig.php
+++ src/Charcoal/Tinify/TinifyConfig.php
@@ -4,6 +4,7 @@ namespace Charcoal\Tinify;
 
 // from 'charcoal-config'
 use Charcoal\Config\AbstractConfig;
+use RuntimeException;
 
 /**
  * Tinify Contrib Module Config
@@ -12,9 +13,18 @@ class TinifyConfig extends AbstractConfig
 {
 
     /**
+     * The max amount of compressions per month.
+     *
      * @var integer $maxCompressions
      */
     private $maxCompressions;
+
+    /**
+     * The path in which to find the files to optimize,
+     *
+     * @var string $basePath
+     */
+    private $basePath;
 
     /**
      * The default data is defined in a JSON file.
@@ -46,6 +56,33 @@ class TinifyConfig extends AbstractConfig
     public function setMaxCompressions($maxCompressions)
     {
         $this->maxCompressions = $maxCompressions;
+
+        return $this;
+    }
+
+    /**
+     * @throws RuntimeException If basePath is missing or not a string.
+     * @return string
+     */
+    public function basePath()
+    {
+        if (isset($this->basePath) || !is_string($this->basePath)) {
+            throw new RuntimeException(sprintf(
+                'Base Path is not set or not of type (string) in [%s]',
+                get_class($this)
+            ));
+        }
+
+        return $this->basePath;
+    }
+
+    /**
+     * @param string $basePath BasePath for TinifyConfig.
+     * @return self
+     */
+    public function setBasePath($basePath)
+    {
+        $this->basePath = $basePath;
 
         return $this;
     }
